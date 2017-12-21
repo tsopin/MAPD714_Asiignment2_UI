e71908cea7853d63c90d8a697bc704a58a93d8cc
Timofei Sopin <timofei.sopin@gmail.com>
2017-12-20 22:31:02 -0500

    Some constraints added

--- A2test/DetailController.swift
+++ A2test/DetailController.swift
@@ -5,23 +5,58 @@
 //  Created by Timofei Sopin on 2017-12-19.
 //  Copyright © 2017 Centennial College. All rights reserved.
 //
-
 import UIKit
+import Foundation
 
-class DetailController: UIViewController {
-
+class DetailController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var label: UILabel!
+    var detailText:String?
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationItem.largeTitleDisplayMode = .never
+        self.textField.delegate = self
+        self.navigationItem.title = detailText 
+    
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let t = detailText {
+            label.text = t
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return (true)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
--- A2test/ViewController.swift
+++ A2test/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let data:[String] = ["Task 1", "Task 2", "Task 3"]
+    var selectedText:String = ""
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedText = data[indexPath.row]
+        self.performSegue(withIdentifier: "showDetail", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let detail = segue.destination as! DetailController
+        detail.detailText = selectedText
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
