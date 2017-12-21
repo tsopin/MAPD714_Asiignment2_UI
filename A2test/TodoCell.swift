11c13c779c9241fb76f14a4e473f748d09512afe
Timofei Sopin <timofei.sopin@gmail.com>
2017-12-20 22:26:02 -0500

    first commit

---
+++ A2test/TodoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoCell.swift
+//  A2test
+//
+//  Created by Timofei Sopin on 2017-12-20.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# MAPD714_Asiignment2_UI
